7156d5964c0b8f1ba2aa530eeca3fe1bd082c4ca
Angkrit2002 <115882709+Angkrit2002@users.noreply.github.com>
2023-02-06 14:46:54 +0700

    aa

---
+++ src/EquTest.java
@@ -0,0 +1,32 @@
+
+public class EquTest {
+	
+	Equ ojb ;
+	
+	@Before
+	
+	public void setup() {
+		ojb = new Equ() ;
+	}
+	
+	@Test
+		public void test01() {
+		
+		assertEquals(25 , ojb.square(5));
+		
+	}
+	
+	@Test
+		public void test02{
+		
+		assertEqulas(ojb.countLetterA("aatrox"),5)
+		
+	}
+	
+	@Test
+		public void test03{
+		
+		assertTrue(ojb.checkTwoLetter("a"))
+		
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author rockk
+ *
+ */
+module Quiz03 {
+	requires junit;
+	requires org.junit.jupiter.api;
+}
